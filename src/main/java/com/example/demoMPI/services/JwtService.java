74c02ce1833992ab9ca7f1240aa59b233e4098e4
raluprodan <Chi1609Fla03>
2025-04-27 19:03:09 +0300

    GM-40 Generated JWT

---
+++ src/main/java/com/example/demoMPI/services/JwtService.java
@@ -0,0 +1,24 @@
+package com.example.demoMPI.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // random secret key generated
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // token valid 10 hours
+                .signWith(key)
+                .compact();
+    }
+}
